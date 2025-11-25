dfbcb9cc93cd57d5cc1731a264d05da872b255a1
uo288066 <uo288066@uniovi.es>
2025-11-25 13:51:21 +0100

    PHP1: ej1: crono.php hecho + incluido en Juegos

--- cronometro.php
+++ cronometro.php
@@ -18,11 +18,79 @@
     <?php
     class Cronometro {
         private $tiempo;
+        private $inicio;
     
         public function __construct() {
-            $this-tiempo = 0;
+            $this->tiempo = 0;
+        }
+
+        public function arrancar() {
+            $this->tiempo = 0;
+            $this->inicio = microtime(true);
+        }
+
+        public function parar() {
+            $fin = microtime(true);
+            $this->tiempo = $fin - $this->inicio;
+        }
+
+        public function mostrar() {
+            $min = floor($this->tiempo / 60);
+            $seg = $this->tiempo - ($min * 60);
+            $formateado = sprintf("%02d:%04.1f", $min, $seg);
+
+            return $formateado;
         }
     }
     ?>
+
+    <header>
+        <h1><a href="index.html">MotoGP Desktop</a></h1>
+
+        <!-- Botón hamburguesa -->
+        <button aria-label="Mostrar/Ocultar menú" aria-expanded="false">&#9776;</button>
+
+        <nav>
+            <a href="index.html" title="Página de inicio">Inicio</a>
+            <a href="piloto.html" title="Información del piloto">Piloto</a>
+            <a href="circuito.html" title="Información del circuito">Circuito</a>
+            <a href="meteorologia.html" title="Información de la meteorología en la próxima semana">Meteorologia</a>
+            <a href="clasificaciones.html" title="Clasificaciones">Clasificaciones</a>
+            <a href="juegos.html" title="Página de juegos" class="active">Juegos</a>
+            <a href="ayuda.html" title="Información de ayuda sobre la página">Ayuda</a>
+        </nav>
+    </header>
+
+    <p>Estás en: <a href="index.html">Inicio</a> | <a href="juegos.html">Juegos</a> | <strong>Cronómetro PHP</strong></p>
+
+    <main>
+        <h2>Cronómetro</h2>
+        <?php
+        session_start();
+
+        if (!isset($_SESSION['crono'])) {
+            $_SESSION['crono'] = new Cronometro();
+        }
+        $crono = $_SESSION['crono'];
+
+        if (count($_POST)>0) {
+            if(isset($_POST['arrancar'])) $crono->arrancar();
+            if(isset($_POST['parar'])) $crono->parar();
+            if(isset($_POST['mostrar'])) $crono->mostrar();
+        }
+
+        echo "<p>Tiempo: " . $crono->mostrar() . "</p>";
+
+        echo "
+            <h3>Pulse un botón</h3>
+            <form action='#' method='post' name='botones'>
+                <input type='submit' name='arrancar' value='arrancar'/>
+                <input type='submit' name='parar' value='parar'/>
+                <input type='submit' name='mostrar' value='mostrar'/>
+            </form>
+        ";
+        ?>
+        
+    </main>
 </body>
 
