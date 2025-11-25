ca315b60653cfa053234e19e423cabcc2f0219c1
uo288066 <uo288066@uniovi.es>
2025-11-25 13:17:33 +0100

    PHP1: ej1 t1,2: crear crono.php + clase Crono

---
+++ cronometro.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML>
+
+<html lang="es">
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="author" content="Olga Alonso Grela"/>
+    <meta name="description" content="Cronómetro"/>
+    <meta name="keywords" content="cronometro, juegos"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="stylesheet" href="estilo/estilo.css"/>
+    <link rel="stylesheet" href="estilo/layout.css"/>
+    <link rel="icon" href="multimedia/img/motogp.ico"/> <!-- HTML2: Ejercicio 3: favicon -->
+    <script src="js/menuMoviles.js"></script>
+    <title>MotoGP-Juegos-Cronómetro</title>
+</head>
+
+<body>
+    <?php
+    class Cronometro {
+        private $tiempo;
+    
+        public function __construct() {
+            $this-tiempo = 0;
+        }
+    }
+    ?>
+</body>
+
